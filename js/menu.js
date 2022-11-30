8522a261bb0af295485dc2c46cc957b729d567e6
Waxer59 <hnboyano@gmail.com>
2022-12-01 00:45:28 +0100

    skypack link added

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,13 @@
 //* Variables
 const menuBtn = document.querySelector('#menu-btn');
 const menu = document.querySelector('#menu');
+const skypackBtn = document.querySelector('#skypack-btn');
 
 //* Functions
 menuBtn.addEventListener('click', () => {
   menu.classList.toggle('menu-active');
+});
+
+skypackBtn.addEventListener('click', () => {
+  window.open('https://www.skypack.dev/', '_blank');
 });
