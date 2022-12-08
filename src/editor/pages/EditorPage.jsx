6b09c09e3e0bddb798e48f62f721ea79276fc1fe
Waxer59 <78129249+Waxer59@users.noreply.github.com>
2022-12-08 12:25:01 +0100

    added snippets to the editor

--- src/editor/pages/EditorPage.jsx
+++ src/editor/pages/EditorPage.jsx
@@ -1,6 +1,7 @@
 import { CodeEditor } from '../components/CodeEditor';
 import { SideBar } from '../components/SideBar';
 import '../helpers/userWorker';
+import '../helpers/editorSnippets';
 
 export const EditorPage = () => {
   return (
