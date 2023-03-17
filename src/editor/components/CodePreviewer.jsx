7de905467d1493813bd2b2d90bf8cc83189f6569
Waxer59 <hnboyano@gmail.com>
2023-03-17 06:28:17 +0100

    remove unused react methods

--- src/editor/components/CodePreviewer.jsx
+++ src/editor/components/CodePreviewer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 import { useCodePreviewer } from '../hooks/useCodePreviewer';
 import { useCodeStore } from '../hooks/useCodeStore';
@@ -6,21 +6,11 @@ import { useCodeStore } from '../hooks/useCodeStore';
 export const CodePreviewer = () => {
   const preview = useRef(null);
   const { activeCode } = useCodeStore();
-  const [code, setCode] = useState(activeCode);
-
   const { update } = useCodePreviewer();
 
   useEffect(() => {
-    update(preview.current, code);
-  }, []);
-
-  useEffect(() => {
-    setCode(activeCode);
+    update(preview.current, activeCode);
   }, [activeCode]);
-
-  useEffect(() => {
-    update(preview.current, code);
-  }, [code]);
 
   return <iframe className="output" ref={preview}></iframe>;
 };
