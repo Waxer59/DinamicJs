a95aaeb1cf0fbba3b4cea174dbc52734ef4e0ba1
Waxer59 <hnboyano@gmail.com>
2023-04-13 02:55:17 +0200

    add auto console log babel plugin

--- src/editor/hooks/useProtectCode.js
+++ src/editor/hooks/useProtectCode.js
@@ -1,13 +1,15 @@
 import * as Babel from '@babel/standalone/babel';
+import autoConsoleLog from 'babel-plugin-auto-console-log';
 import protect from 'loop-protect';
 
 const TIMEOUT = 100;
 
 Babel.registerPlugin('loopProtection', protect(TIMEOUT));
+Babel.registerPlugin('autoConsoleLog', autoConsoleLog());
 export const useProtectCode = () => {
   const transform = (source) =>
     Babel.transform(source, {
-      plugins: ['loopProtection']
+      plugins: ['autoConsoleLog', 'loopProtection']
     }).code;
 
   const protectCode = (code) => {
